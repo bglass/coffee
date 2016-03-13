cdc08e304fbdc4963fc5ec74bfe4ba8a365f76cf
Boris Glass <github@com.glass.li>
2016-03-13 22:52:56 +0100

    dynamic button creation

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,10 +1,6 @@
-addAnother = function() {
-    var group = document.getElementById("grouplist");
-    var button = document.createElement("button");
-    var children = group.children.length + 1
-    button.setAttribute("id", "element"+children)
-    button.setAttribute("class", "ui-btn ui-corner-all")
-
-    button.appendChild(document.createTextNode("Element "+children));
-    group.appendChild(button)
-}
+$('#peopleButtons').on('click', 'button', function(event) {
+  var name = this.textContent
+  var r = $('<input type="button" value="' + name + '">');
+    console.log ( '#someButton was clicked ' + name );
+  $("div#groupButtons").append(r);
+});
